166e25efdd5744168f31b7062f226c7d5f6a8ebb
Hug00x <111132271+Hug00x@users.noreply.github.com>
2024-12-30 21:27:08 +0000

    Update ProdutoRoupas.java

--- ProdutoRoupas.java
+++ ProdutoRoupas.java
@@ -1,8 +1,9 @@
 public class ProdutoRoupas extends Produto{
+	private static final long serialVersionUID = 1L;
 	private String tamanho;
 	private String genero;
 	
-	public ProdutoRoupas(Produto p, String id, double preco, String tamanho, String genero, String marca) {
+	public ProdutoRoupas(Produto p, String id, double preco, String tamanho, String genero) {
 		super(p.getNome(), p.getDescricao(), p.getPreco(), p.getCategoria(), p.getAtivo());
 		this.tamanho = tamanho;
 		this.genero = genero;
@@ -23,14 +24,11 @@ public class ProdutoRoupas extends Produto{
 	public void setGenero(String genero) {
 		this.genero = genero;
 	}
-	
 	@Override
 	public String toString() {
-		return "ProdutoRoupas{" +
-                ", preco=" + getPreco() +
-                ", tamanho='" + tamanho + '\'' +
-                ", genero='" + genero + '\'' +
-                '}';
+	    return super.toString() + "\n" + 
+	           "  Tamanho: " + tamanho + "\n" +
+	           "  Género: " + genero;
 	}
 
 }
